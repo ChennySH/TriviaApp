f2d4185e5f3b323783d51dd6ed61e822edf6640d
chenny shahir <chenny.shahir@gmail.com>
2021-05-05 13:20:34 +0300

    TriviaProject

---
+++ TriviaApp/TriviaApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TriviaApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TriviaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+            //MainPage = new NavigationPage(new LogInPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
