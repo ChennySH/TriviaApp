3fb1e6055efe5d963361c4b735f008f4f5f87020
chenny shahir <chenny.shahir@gmail.com>
2021-05-13 17:27:02 +0300

    sync

---
+++ TriviaApp/TriviaApp/Services/TriviaAppWebApiProxy.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TriviaApp.Models;
+
+using System.Text.Json;
+
+namespace TriviaApp.Services
+{
+    class TriviaAppWebApiProxy
+    {
+        private HttpClient client;
+        private string baseUri;
+        private static TriviaAppWebApiProxy proxy = null;
+
+
+        public static TriviaAppWebApiProxy CreateProxy(string baseUri = "https://ramonws.azurewebsites.net/AmericanQuestions/")
+        {
+            if (proxy == null)
+                proxy = new TriviaAppWebApiProxy(baseUri);
+            return proxy;
+        }
+        private TriviaAppWebApiProxy(string baseUri)
+        {
+            //Set client handler to support cookies!!
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = new System.Net.CookieContainer();
+
+            //Create client with the handler!
+            this.client = new HttpClient(handler, true);
+            this.baseUri = baseUri;
+        }
+        //Login Method
+        public async Task<User> LoginAsync(string email, string password)
+        {
+            try
+            {
+                HttpResponseMessage respose = await this.client.GetAsync($"{this.baseUri}/Login?email={email}&pass={password}");
+                if (respose.IsSuccessStatusCode)
+                {
+                    JsonSerializerOptions options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    string content = await respose.Content.ReadAsStringAsync();
+                    User user = JsonSerializer.Deserialize<User>(content, options);
+                    return user;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        // Register Method
+        public async Task<bool> RegisterAsync(User u)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                string jsonUser = JsonSerializer.Serialize<User>(u, options);
+                StringContent content = new StringContent(jsonUser, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await this.client.PostAsync($"{this.baseUri}/RegisterUser", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonContext = await response.Content.ReadAsStringAsync();
+                    bool b = JsonSerializer.Deserialize<bool>(jsonContext, options);
+                    return b;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            
+        }
+        // Get Random Question Method
+        public async Task<AmericanQuestion> GetRandomQuestionAsync()
+        {
+            try
+            {
+                HttpResponseMessage respnose = await this.client.GetAsync($"{this.baseUri}/GetRandomQuestion");
+                if (respnose.IsSuccessStatusCode)
+                {
+                    JsonSerializerOptions options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                    string content = await respnose.Content.ReadAsStringAsync();
+                    AmericanQuestion question = JsonSerializer.Deserialize<AmericanQuestion>(content, options);
+                    return question;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }            
+        }
+        // Post New Question Method
+        public async Task<bool> PostNewQuestionAsync(AmericanQuestion q)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                string jsonQuestion = JsonSerializer.Serialize<AmericanQuestion>(q, options);
+                StringContent content = new StringContent(jsonQuestion, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await this.client.PostAsync($"{baseUri}/PostNewQuestion", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonContent = await response.Content.ReadAsStringAsync();
+                    bool b = JsonSerializer.Deserialize<bool>(jsonContent, options);
+                    return b;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+        // Delete Question Meth
+        public async Task<bool> DeleteQuestionAsync(AmericanQuestion q)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                string jsonQuestion = JsonSerializer.Serialize<AmericanQuestion>(q, options);
+                StringContent content = new StringContent(jsonQuestion, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await this.client.PostAsync($"{this.baseUri}/DeleteQuestion", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonContent = await response.Content.ReadAsStringAsync();
+                    bool b = JsonSerializer.Deserialize<bool>(jsonContent, options);
+                    return b;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+        
+    }
+    
+}
---
+++ TriviaApp/TriviaApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using TriviaApp.Models;
+using Xamarin.Forms;
+using TriviaApp.Services;
+using System.Threading.Tasks;
+
+namespace TriviaApp.ViewModels
+{
+    class LoginPageViewModel : INotifyPropertyChanged
+    {
+        public LoginPageViewModel()
+        {
+            this.email = "";
+            this.password = "";
+            currentUser = null;
+            proxy = TriviaAppWebApiProxy.CreateProxy();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private TriviaAppWebApiProxy proxy;
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                if (value != this.email)
+                {
+                    this.email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private User currentUser;
+        public ICommand LoginCommand => new Command(LoginMethod);
+
+        private async void LoginMethod()
+        {
+            if(this.email != "" && this.password != "" && currentUser == null)
+            {
+                currentUser = await this.proxy.LoginAsync(this.email, this.password);
+            }
+            if(currentUser != null)
+            {
+                
+            }
+        }
+    }
+}
